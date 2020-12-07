e1da9c61d6d46a230a516dae671ad91695761980
yusuke hata <y-hata@selfree.co.jp>
2020-12-08 00:30:30 +0900

    seedの作成途中

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  User.create!(nickname: "ユーザー#{i+1}",
+              email: "ippan#{i+1}@user.com",
+              password: "password02",
+              password_confirmation: "password02"
+              )
+end
+
+# 迷子
+Pet.create!(name: "太郎",
+            kind: "オカメインコ",
+            gender: "オス,
+            feature: "羽の抜けやすい子です。",
+            image: open("./db/fixtures/pet1.jpg"),
+            status: 1,
+            lost_day: Time.zone.now,
+            prefecture: 1,
+            place: "モエレ沼公園",
+            area: "北海道 モエレ沼公園",
+            latitude: 43.1225258,
+            longitude: 141.4307835,
+            wanted: true
+            )
